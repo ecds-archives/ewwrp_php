79b16884f9988806a2cbabff3c9443b5add1b8a5
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-06 23:10:23 +0000

    Initial revision

---
+++ genrefiction/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$title = "Women's Genre Fiction Project";
+$collname = "Genre Fiction Collection";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../index.php");
+
+
+?>
