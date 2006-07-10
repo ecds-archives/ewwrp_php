12b7a8fbb68d3fb71e33976b6f5e6876a0de52d7
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-10 22:17:02 +0000

    genre fiction version of pages; all functionality is in main ewwrp version

---
+++ genrefiction/content.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = "Women's Genre Fiction Project";
+$collname = "Genre Fiction Collection";
+$collection = "Genre Fiction";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../content.php");
+
+
+?>
