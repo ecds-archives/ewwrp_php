7fa556af371e1603d599ebecb33e55e4c02dfaf1
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-10 22:18:37 +0000

    copied from Genre Fiction & minor changes made for Native American collection

---
+++ trunk/nativeamerican/toc.php
@@ -0,0 +1,13 @@
+<?php
+
+$collname = "Native American Collection";
+$title = $collname;
+$collection = "Native American";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../toc.php");
+
+
+?>
