aaa3eff1170ccbae939da33a77330028fc74cf76
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-10 22:15:12 +0000

    links to collection relative to site; form works for keyword search

--- trunk/nav.php
+++ trunk/nav.php
@@ -9,7 +9,7 @@
 </div>
 
 <div id="logo">
-<a id="ewwrp" href="http://chaucer.library.emory.edu/wwrp/">
+<a id="ewwrp" href="<?= $baseurl ?>">
   <img src="<?= $baseurl ?>images/ewwrp.png" name="ewwrp" alt="Emory Women Writers Resource Project"
   border="0" height="74" width="56"></a>
 </div>
@@ -21,12 +21,9 @@
   <p id="m-search">- <a>Search</a> <?=$collname ?></p>
 
   
-<form class="menu" action="frames?contentsrc=search" method="post">
-    
-<input name="visited" value="true" type="hidden">
-    <input name="quantity" value="60" type="hidden"> 
-    <input name="content" size="20" value="" align="left" type="text">
-    <input onmouseover='src="<?= $baseurl ?>images/search-on.jpg"' onmouseout='src="<?= $baseurl ?>images/search-off.jpg"' src="<?= $baseurl ?>images/search-off.jpg" name="searchbutton" alt="search" type="image">
+<form class="menu" action="search.php" method="GET">
+  <input name="keyword" size="20" value="" align="left" type="text">
+  <input onmouseover='src="<?= $baseurl ?>images/search-on.jpg"' onmouseout='src="<?= $baseurl ?>images/search-off.jpg"' src="<?= $baseurl ?>images/search-off.jpg" name="searchbutton" alt="search" type="image">
   </form>
     
 <div class="advsearch">
@@ -47,10 +44,10 @@
 <div class="collections">
       <b><? if ($collname != 'EWWRP') echo "Other "?>Emory Women Writers Resource Project Collections:</b>
 <ul>
- <li>- <a id="genrefiction" href="genrefiction/">Genre Fiction</a></li>
+ <li>- <a id="genrefiction" href="<?= $baseurl ?>genrefiction/">Genre Fiction</a></li>
  <li>- <a id="earlymodern" href="http://chaucer.library.emory.edu/wwrp/">Early Modern</a></li>
  <li>- <a id="worldwar1" href="http://chaucer.library.emory.edu/wwrp/">World War I Poetry</a></li>
- <li>- <a id="nativeamerican" 	href="http://chaucer.library.emory.edu/wwrp/">Native
+ <li>- <a id="nativeamerican" 	href="<?= $baseurl ?>nativeamerican/">Native
 	American</a></li>
  <li>- <a id="africanamerican"	href="http://chaucer.library.emory.edu/wwrp/">African
 	American</a></li>
