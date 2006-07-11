af19bed7f7ff9f4f70e62ed5b67ca6d277b70cd8
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-11 21:22:17 +0000

    added collection name - to filter front page images

--- genrefiction/index.php
+++ genrefiction/index.php
@@ -2,6 +2,7 @@
 
 $title = "Women's Genre Fiction Project";
 $collname = "Genre Fiction Collection";
+$collection = "Genre Fiction";
 
 // add parent directory to path
 set_include_path(get_include_path() . ":..");
