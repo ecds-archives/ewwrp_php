34cbe44734c5e3a77f77aadb42512d9221c9fa38
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-12 00:58:24 +0000

    correctly handles sections (display first div/titlePage and TOC for section) next/prev nav within section working; display format is "chapter #"

--- content.php
+++ content.php
@@ -14,6 +14,7 @@ $baseurl = "http://biliku.library.emory.edu/rebecca/ewwrp/";
 
 $id = $_GET["id"];
 $node = $_GET["level"];
+$runninghdr = $_GET["running-header"];
 // need a filter if we are in a collection ?
 
 if ($title == '') {
@@ -23,25 +24,50 @@ if ($title == '') {
 
 
 $query = "let \$a := //${node}[@id='$id']
+let \$doc := substring-before(util:document-name(\$a), '.xml')
 let \$root := root(\$a)
+let \$content := (if (exists(\$a/div|\$a/titlePage)) then
+  <content>{(\$a/div|\$a/titlePage)[1]}</content>
+else 
+  <content>
+  {\$a/preceding-sibling::pb[1]}{\$a}
+  </content>)
 return <TEI.2>
+  <doc>{\$doc}</doc>
   <teiHeader>
     {\$root//teiHeader//titleStmt}
     {\$root//sourceDesc}
   </teiHeader>
   <relative-toc>
-    {for \$d in \$root//(TEI.2|front|body|back|text|group|div)[exists(.//div/@id='$id')]
+    {for \$d in (\$a/ancestor::TEI.2|\$a/ancestor::front|\$a/ancestor::titlePage|\$a/ancestor::body|\$a/ancestor::back|\$a/ancestor::text|\$a/ancestor::group|\$a/ancestor::div|\$a|\$a/div[1])
      return <item name='{name(\$d)}'>{\$d/@*}{\$d/head}
         <parent>{\$d/../@id}{name(\$d/..)}</parent>
       </item>}
   </relative-toc>
-  <content>{\$a}</content>
+  <toc>{for \$i in \$a//(front|body|back|text|group|titlePage|div)[@id!='$id']
+      return <toc-item name='{name(\$i)}'>{\$i/@*}{\$i/head}
+      <parent>{\$i/../@id}{name(\$i/..)}</parent>
+     </toc-item>}
+  </toc>
+  <nav>
+    {for \$s in (\$a/preceding-sibling::div[last()]|\$a/preceding-sibling::titlePage[last()])[1]
+ 	return <first name='{name(\$s)}'>{\$s/@*}</first>}
+    {for \$s in (\$a/preceding-sibling::div[1]|\$a/preceding-sibling::titlePage[1])[last()]
+ 	return <prev name='{name(\$s)}'>{\$s/@*}</prev>}
+    {for \$s in (\$a/following-sibling::div[1]|\$a/following-sibling::titlePage[1])[1]
+ 	return <next name='{name(\$s)}'>{\$s/@*}</next>}
+    {for \$s in (\$a/following-sibling::div[last()]|\$a/following-sibling::titlePage[last()])[1]
+ 	return <last name='{name(\$s)}'>{\$s/@*}</last>}
+  </nav>
+  {\$content}
 </TEI.2>
 ";
 
 
 
 $xsl = "$baseurl/stylesheets/content.xsl";
+$xsl_params = array('url' => "content.php?level=$node&id=$id");
+if($runninghdr) $xsl_params{"running-header"} = $runninghdr;
 
 
 ?>
@@ -62,10 +88,9 @@ $xsl = "$baseurl/stylesheets/content.xsl";
 
 <? 
 $db->xquery($query);
-$db->xslTransform($xsl);
+$db->xslTransform($xsl, $xsl_params);
 $db->printResult();
 ?>
-
 
 </div>	
 
