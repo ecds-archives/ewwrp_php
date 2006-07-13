d988b9352db38b6ac6b66d6177daac3168809000
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-13 20:39:48 +0000

    all pages for abolition, freedom, & rights collection

---
+++ abolition/toc.php
@@ -0,0 +1,13 @@
+<?php
+
+$collection = "Abolition, Freedom, and Rights";
+$title = "$collection Collection";
+$collname = $title;
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../toc.php");
+
+
+?>
