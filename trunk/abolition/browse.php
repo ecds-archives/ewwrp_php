1d88363ca45467ea99a696f382de41d89728d24c
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-13 20:39:48 +0000

    all pages for abolition, freedom, & rights collection

---
+++ trunk/abolition/browse.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = "Abolition, Freedom, and Rights Collection";
+$collname = "Abolition, Freedom, and Rights Collection";
+$collection = "Abolition, Freedom, and Rights";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../browse.php");
+
+
+?>
---
+++ trunk/abolition/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = "Abolition, Freedom, and|Rights Collection";	// | determines where to center text
+$collname = "Abolition, Freedom, and Rights Collection";
+$collection = "Abolition, Freedom, and Rights";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+include("../index.php");
+
+
+?>
