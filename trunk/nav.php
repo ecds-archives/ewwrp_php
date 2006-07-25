ef87e14ff4a56530675b2f43eef4129abda4d36d
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-25 19:46:19 +0000

    small changes, formatting

--- trunk/nav.php
+++ trunk/nav.php
@@ -22,8 +22,11 @@
 
   
 <form class="menu" action="search.php" method="GET">
-  <input name="keyword" size="20" value="" align="left" type="text">
-  <input onmouseover='src="<?= $baseurl ?>images/search-on.jpg"' onmouseout='src="<?= $baseurl ?>images/search-off.jpg"' src="<?= $baseurl ?>images/search-off.jpg" name="searchbutton" alt="search" type="image">
+ <input name="keyword" size="20" value="" align="left" type="text">
+    <input onmouseover='src="<?= $baseurl ?>images/search-on.jpg"' onmouseout='src="<?= $baseurl ?>images/search-off.jpg"' src="<?= $baseurl ?>images/search-off.jpg" name="searchbutton" alt="search" type="image">
+   <!-- to control search button with css and avoid search.x + search.y, use something like this -->
+<!-- <a><input id="search" type="submit" value="Search" alt="search"></a> -->
+
   </form>
     
 <div class="advsearch">
@@ -42,15 +45,14 @@
 <hr class="menu">
 
 <div class="collections">
-      <b><? if ($collname != 'EWWRP') echo "Other "?>Emory Women Writers Resource Project Collections:</b>
+      <b>Emory Women Writers Resource Project Collections:</b>
 <ul>
  <li>- <a id="genrefiction" href="<?= $baseurl ?>genrefiction/">Genre Fiction</a></li>
  <li>- <a id="earlymodern" href="http://chaucer.library.emory.edu/wwrp/">Early Modern</a></li>
  <li>- <a id="worldwar1" href="http://chaucer.library.emory.edu/wwrp/">World War I Poetry</a></li>
  <li>- <a id="nativeamerican" 	href="<?= $baseurl ?>nativeamerican/">Native
 	American</a></li>
- <li>- <a id="africanamerican"	href="http://chaucer.library.emory.edu/wwrp/">African
-	American</a></li>
+ <li>- <a id="abolition" href="<?= $baseurl ?>abolition">Abolition, Freedom, and Rights</a></li>
  <li>- <a id="advocacy"	href="http://chaucer.library.emory.edu/wwrp/">Women's
 	    Advocacy</a></li>
 </ul>
