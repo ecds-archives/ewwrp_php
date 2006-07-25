fb5350bc5ad165c16da3fa9997396f979a3fc83e
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-25 19:46:50 +0000

    added doctitle to html header

--- kwic.php
+++ kwic.php
@@ -32,7 +32,11 @@ if ($title == '') {
 $xquery = "let \$doc := document('db/$db/$docname.xml')/TEI.2
 return <TEI.2>
 {\$doc/@id}
-{\$doc/@teiHeader}
+<doc>$docname</doc>
+<teiHeader>
+  {\$doc//teiHeader//titleStmt}
+  {\$doc//sourceDesc}
+</teiHeader>
 {for \$a in \$doc//(front|body|back|text|group|titlePage|div)[. |= '$keyword' or .//* |= '$keyword']
   return <item name='{name(\$a)}'>{\$a/@*}{\$a/head}
   {\$a/front/titlePage/docTitle/titlePart[@type='main']}
@@ -52,11 +56,15 @@ return <TEI.2>
    above is another way-- allow any node, but if the node is a <hi>, return parent instead
    (what other nodes would need to be excluded? title? others?)
 */
-print '<html>
+
+$xdb->xquery($xquery);
+$doctitle = $xdb->findnode("title");
+
+print "<html>
  <head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="ewwrp.css">
-    <link rel="shortcut icon" href="ewwrp.ico" type="image/x-icon">';
+    <title>$title : $doctitle : Keyword in Context</title>
+    <link rel='stylesheet' type='text/css' href='ewwrp.css'>
+    <link rel='shortcut icon' href='ewwrp.ico' type='image/x-icon'>";
 
 include("header.php");
 include("nav.php");
@@ -68,7 +76,6 @@ print "<div class='content'>
 $xdb->xslBind("$baseurl/stylesheets/kwic-towords.xsl");
 $xdb->xslBind("$baseurl/stylesheets/kwic.xsl");
 
-$xdb->xquery($xquery);
 $xdb->transform();
 $xdb->printResult();
 
