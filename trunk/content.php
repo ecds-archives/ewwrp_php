0e2766c3f82ba511823ca861df90f4b0720eca46
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-26 23:04:09 +0000

    added full-size pageimage to range of content to be displayed

--- trunk/content.php
+++ trunk/content.php
@@ -15,6 +15,8 @@ $baseurl = "http://biliku.library.emory.edu/rebecca/ewwrp/";
 $id = $_GET["id"];
 $node = $_GET["level"];
 $runninghdr = $_GET["running-header"];
+$keyword = $_GET["keyword"];
+$view = $_GET["view"];		// print, blackboard, ??
 // need a filter if we are in a collection ?
 
 if ($title == '') {
@@ -26,11 +28,22 @@ if ($title == '') {
 $query = "let \$a := //${node}[@id='$id']
 let \$doc := substring-before(util:document-name(\$a), '.xml')
 let \$root := root(\$a)
-let \$content := (if (exists(\$a//div|\$a//titlePage)) then
-  <content>{((\$a/div[not(./div)],\$a//div[not(./div)])|\$a//titlePage)[1]}</content>
-else 
-  <content>
-  {\$a/preceding-sibling::pb[1]}{\$a}
+let \$contentnode := (if (exists(\$a//div|\$a//titlePage)) then
+  ((\$a/div[not(./div)],\$a//div[not(./div)])|\$a//titlePage)[1]
+else
+  \$a)
+let \$contentnodehi := \$contentnode[. |= '$keyword']
+let \$content := (if (exists(\$contentnodehi)) then
+ <content>{\$contentnodehi}</content>
+else
+ <content>
+  {\$contentnode}
+";
+// don't return preceding pb if main item is itself a pb
+// note: sibling pb is not showing up in some cases where it should 
+//$query .=   ($node != "pb") ? "{\$a/preceding-sibling::pb[1]}" : "";
+$query .= "
+
   </content>)
 return <TEI.2>
   <doc>{\$doc}</doc>
@@ -49,7 +62,22 @@ return <TEI.2>
       return <item name='{name(\$i)}'>{\$i/@*}{\$i/head}
       <parent>{\$i/../@id}{name(\$i/..)}</parent>
      </item>}
-  </toc>
+  </toc>";
+
+if ($node == "pb") {
+  $query .=   "<nav>
+    {for \$s in \$a/preceding::pb[@entity][last()]
+ 	return <first name='{name(\$s)}'>{\$s/@*}</first>}
+    {for \$s in \$a/preceding::pb[@entity][1]
+ 	return <prev name='{name(\$s)}'>{\$s/@*}</prev>}
+    {for \$s in \$a/following::pb[@entity][1]
+ 	return <next name='{name(\$s)}'>{\$s/@*}</next>}
+    {for \$s in \$a/following::pb[@entity][last()]
+ 	return <last name='{name(\$s)}'>{\$s/@*}</last>}
+  </nav>";
+
+} else {
+  $query .= "
   <nav>
     {for \$s in (\$a/preceding-sibling::div[last()]|\$a/preceding-sibling::titlePage[last()]|\$a/preceding-sibling::text[last()])[1]
  	return <first name='{name(\$s)}'>{\$s/@*}
@@ -67,37 +95,72 @@ return <TEI.2>
  	return <last name='{name(\$s)}'>{\$s/@*}
 	  {\$s/front/titlePage/docTitle/titlePart[@type='main']}
 	</last>}
-  </nav>
+  </nav>";
+}
+$query .= "
   {\$content}
 </TEI.2>
 ";
 
-
+// add keyword parameter to url, if there is one defined
+$kwurl = ($keyword != '') ? "keyword=$keyword" : "";
 
 $xsl = "$baseurl/stylesheets/content.xsl";
-$xsl_params = array("url" => "content.php?level=$node&id=$id",
+$xsl_params = array("url" => "content.php?level=$node&id=$id&$kwurl",	// FIXME: gets blank & if no keyword
+		    "url_suffix" => $kwurl,
 		    "node" => $node, "id" => $id);
 if($runninghdr) $xsl_params{"running-header"} = $runninghdr;
 
-
-?>
-<html>
- <head>
-    <title><?= $title ?></title>
-    <link rel="stylesheet" type="text/css" href="ewwrp.css">
-    <link rel="shortcut icon" href="ewwrp.ico" type="image/x-icon">
-</head>
-<body>
-
-<? include("header.php") ?>
-<? include("nav.php") ?>
-
-<div class="content">
-
-<div class="title"><a href="index.php"><?= $title ?></a></div>
-
-<? 
 $db->xquery($query);
+
+// extract information to use in html page title
+
+$doctitle = $db->findnode("title");
+// truncate document title for html header
+$doctitle = str_replace(", an electronic edition", "", $doctitle);
+
+// generate a title appropriate to the kind of content being displayed
+switch ($node) {
+ case "div":
+   $content_title =  $db->findnode("content/div/@type") . " " .  $db->findnode("content/div/@n");
+   break;
+ case "titlePage": $content_title = "title page"; break;
+ case "pb": $content_title = $db->findnode("content/pb/@pages");
+   if (!(strstr($content_title, "page"))) $content_title = "Page $content_title";
+ }
+
+
+// if we are in a collection, add EWWRP to the beginning of the html title
+if ($collname != "EWWRP") 
+  $htmltitle = "EWWRP : $title";
+else
+   $htmltitle = $title;
+
+
+print "<html>
+ <head>
+    <title>$htmltitle : $doctitle : $content_title</title>
+";
+switch ($view) {
+ case "print": 
+ case "blackboard":
+   print "<link rel='stylesheet' type='text/css' href='$baseurl/$view.css'>";
+   break;
+ default: print "<link rel='stylesheet' type='text/css' href='ewwrp.css'>"; 
+ }
+
+print "
+    <link rel='shortcut icon' href='ewwrp.ico' type='image/x-icon'>
+</head>
+<body>";
+
+include("header.php");
+include("nav.php");
+
+print '<div class="content">';
+
+print "<div class='title'><a href='index.php'>$title</a></div>";
+
 $db->xslTransform($xsl, $xsl_params);
 $db->printResult();
 ?>
