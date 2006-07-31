0093c51c16d9d89e16e5e61cb37159a4171aae26
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-31 22:15:43 +0000

    added advanced/multi-field search

--- search.php
+++ search.php
@@ -12,11 +12,15 @@ global $collection;
 
 $baseurl = "http://biliku.library.emory.edu/rebecca/ewwrp/";	
 
-$kw = $_GET["keyword"];
+$kw = $_REQUEST["keyword"];
+$doctitle = $_REQUEST["doctitle"];
+$auth = $_REQUEST["author"];
+$date = $_REQUEST["date"];
+$subj = $_REQUEST["subject"];
 // need a filter if we are in a collection
 
-$pos = $_GET["position"];
-$max = $_GET["max"];
+$pos = $_REQUEST["position"];
+$max = $_REQUEST["max"];
 
 if ($pos == '') $pos = 1;
 if ($max == '') $max = 20;
@@ -39,20 +43,39 @@ if ($collection) {
   $rsfilter = "[teiHeader/profileDesc/creation/rs[@type='collection' and .='$collection']]";
 }
 
-$query = "for \$a in /TEI.2${rsfilter}[. &= '$kw']
+$options = array();
+if ($kw) 
+  array_push($options, ". &='$kw'");
+if ($doctitle)
+  array_push($options, ".//titleStmt/title &= '$doctitle'");
+if ($auth)
+  array_push($options, "(.//titleStmt/author/name &= '$auth' or .//titleStmt/author/name/@reg &= '$auth')");
+if ($date)
+  array_push($options, ".//sourceDesc/bibl/date &= '$date'");
+if ($subj)
+  array_push($options, ".//keywords/list/item &= '$subj'");
+
+$searchfilter = "[" . implode(" and ", $options) . "]"; 
+
+
+$query = "for \$a in /TEI.2${rsfilter}$searchfilter
 let \$t := \$a//titleStmt/title
 let \$doc := substring-before(util:document-name(\$a), '.xml')
 let \$auth := \$a//titleStmt/author
 let \$date := root(\$a)//sourceDesc/bibl/date
 let \$matchcount := text:match-count(\$a)
 order by \$matchcount descending
-return <item>{\$a/@id}
-  <hits>{\$matchcount}</hits>
+return <item>{\$a/@id}";
+if ($kw)	// only count matches for keyword searches
+  $query .= "<hits>{\$matchcount}</hits>";
+$query .= "
   {\$t}
   <id>{\$doc}</id>
   {\$auth}
-  {\$date}
-</item>";
+  {\$date}";
+if ($subj)	// return subjects if included in search 
+  $query .= "{for \$s in \$a//keywords/list/item return <subject>{string(\$s)}</subject>}";
+$query .= "</item>";
 
 $xsl = "$baseurl/stylesheets/browse.xsl";
 //$xsl_params = array('field' => $field, 'value' => $value, 'max' => $max);
@@ -78,7 +101,26 @@ $xsl_params = array('mode' => "search", 'keyword' => $kw, 'max' => $max);
 
 $db->xquery($query, $pos, $max);
 
-print "<h1>Texts that contain '$kw'</h1>";
+print "<p><b>Search results for texts where:</b></p>
+ <ul class='searchopts'>";
+if ($kw) 
+  print "<li>document contains keywords '$kw'</li>";
+if ($doctitle)
+  print "<li>title matches '$doctitle'</li>";
+if ($auth)
+  print "<li>author matches '$auth'</li>";
+if ($date)
+  print "<li>date matches '$date'</li>";
+if ($subj)
+  print "<li>subject matches '$subj'</li>";
+
+print "</ul>";
+
+if ($db->count == 0) {
+  print "<p><b>No matches found.</b>
+You may want to broaden your search or consult the search tips for suggestions.</p>\n";
+  include("searchform.php");
+ }
 
 $db->xslTransform($xsl, $xsl_params);
 $db->printResult();
