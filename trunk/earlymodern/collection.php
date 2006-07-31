8fabd0c3a579e264739c576a4d0305ce4dca8ad3
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-31 21:18:10 +0000

    files for early modern collection of ewwrp

---
+++ trunk/earlymodern/collection.php
@@ -0,0 +1,12 @@
+<?php
+
+// settings for this collection (used by all content pages)
+
+$title = "Early Modern";
+$collection = "Early Modern through the 18th Century";
+$collname = "$title Collection";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+?>
