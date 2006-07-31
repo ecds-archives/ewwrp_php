394a24312d7220b8a87dd334298d8513077075db
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-31 21:18:47 +0000

    files for world war I collection of ewwrp

---
+++ trunk/worldwarI/collection.php
@@ -0,0 +1,12 @@
+<?php
+
+// settings for this collection (used by all content pages)
+
+$title = "World War I Poetry";
+$collection = "World War I";
+$collname = "$title Collection";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+?>
