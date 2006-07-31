a8108c2bdda915fcd6dcbc987ece7cb8f697174f
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-07-31 21:17:37 +0000

    files for the women's advocacy collection of the ewwrp

---
+++ advocacy/collection.php
@@ -0,0 +1,12 @@
+<?php
+
+// settings for this collection (used by all content pages)
+
+$title = "Women's Advocacy";
+$collection = "Women\'s Advocacy";
+$collname = "$title Collection";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+?>
