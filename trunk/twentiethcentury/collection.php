45201a163537d624571e2a5007894d433c774c92
rsutton <rsutton@1ab97bbd-5a3a-0410-acfa-a04729158354>
2006-07-31 21:18:32 +0000

    files for twentieth century collection of ewwrp

---
+++ trunk/twentiethcentury/collection.php
@@ -0,0 +1,12 @@
+<?php
+
+// settings for this collection (used by all content pages)
+
+$title = "Early 20th Century";
+$collection = "Early 20th Century Literature";
+$collname = "$title Collection";
+
+// add parent directory to path
+set_include_path(get_include_path() . ":..");
+
+?>
