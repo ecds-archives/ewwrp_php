171ef4a2bca0f0cb772c5ce56041375529c2e08f
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-12-12 22:57:04 +0000

    advanced search - filter by collection(s)

--- searchform.php
+++ searchform.php
@@ -20,6 +20,27 @@
    <td class="input">
      <input type="text" size="40" name="subject" value="<?php print $subj ?>">
     </td></tr>
+<?php
+if ($collname == "EWWRP") {
+  print '
+<tr>
+  <th>Collection(s)</th>
+  <td class="input">
+    <select multiple name="coll[]" size="8">
+	<option value="ALL" selected="true">ALL</option>
+	<option value="Genre Fiction">Genre Fiction</option>
+	<option value="Early Modern through the 18th Century">Early Modern through the 18th Century</option>
+	<option value="Early 20th Century Literature">Early 20th Century Literature</option>
+	<option value="World War I">World War I</option>
+	<option value="Native American">Native American</option>
+	<option value="Abolition, Freedom, and Rights">Abolition, Freedom, and Rights</option>
+	<option value="Women\'s Advocacy">Women\'s Advocacy</option>
+    </select>
+  </td></tr>
+';
+}
+?>
+
 <tr><td colspan="2">
 
   <input onmouseover='src="<?= $baseurl ?>images/search-on.jpg"' onmouseout='src="<?= $baseurl ?>images/search-off.jpg"' src="<?= $baseurl ?>images/search-off.jpg" name="searchbutton" alt="search" type="image">
